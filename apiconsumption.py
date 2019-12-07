e554462c48297f56b0c219d7e0ac53cb11163bf8
VintageBadger <sleepypandas2@gmail.com>
2019-12-07 12:16:33 -0600

    Error Messages

--- apiconsumption.py
+++ apiconsumption.py
@@ -1,4 +1,18 @@
 import argparse
+import requests
+
+def lookup(route, stopName, direction):
+    try:
+        r = requests.get('http://svc-api:47101/api/Routes')
+        if r.status_code == 200:
+            print(r.json())
+    except Exception as ex:
+        print(ex)
+
+
+
+
+
 
 if __name__ == '__main__':
     #expecting 3 args
@@ -10,5 +24,6 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     print(args.route + " " + args.stopName + " " + args.direction)
+    lookup(args.route, args.stopName, args.direction)
     #return time in minutes
     print("return not functional yet")
